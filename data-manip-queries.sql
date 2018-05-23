a776fe0f7375d711e032d94e01c49acda2234f8c
arbruske <aleksander.bruske@gmail.com>
2018-05-22 22:12:25 -0500

    Update data-manip-queries.sql

--- data-manip-queries.sql
+++ data-manip-queries.sql
@@ -1,9 +1,5 @@
 -- Get names and ids of all Customers
 SELECT customer_id, first_name, last_name FROM proj_customers
 
--- get all customer's data to populate a dropdown for associating with a product
-
-SELECT customer_id AS cid, first_name, last_name FROM proj_customers
-
 -- add a new customer
 INSERT INTO proj_customers(first_name, last_name) VALUES ([fname_input], [lname_input])
