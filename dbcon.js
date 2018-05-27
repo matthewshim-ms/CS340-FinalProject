2c55448a88523e47a01ded1bb463ab0c8a18f734
arbruske <aleksander.bruske@gmail.com>
2018-05-27 00:21:10 -0500

    added things

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_shimm',
-  password        : '2344',
-  database        : 'cs340_shimm'
+  user            : 'cs340_bruskea',
+  password        : '5914',
+  database        : 'cs340_bruskea'
 });
 
 module.exports.pool = pool;
